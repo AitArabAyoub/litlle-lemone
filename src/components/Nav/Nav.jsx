f4dc1d0f774de3de38a04fce322a0b8a12759534
aitarabayoub <ayoubaitarab20@gmail.com>
2024-03-17 20:07:56 +0100

    c1

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../../images/Asset 9@4x.png'
+function Nav() {
+    return (
+        <nav>
+            <img src={logo} alt="" width={"80px"}/>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
